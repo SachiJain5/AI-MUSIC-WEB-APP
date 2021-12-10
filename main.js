0a09414c198a502bea2a0a85e367c5d7d6689a5f
SachiJain5 <73456720+SachiJain5@users.noreply.github.com>
2021-12-10 19:53:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
     song1 = loadSound("song1.mp3");
@@ -12,8 +16,26 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+    console.log('poseNet is initialize')
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+function gotPoses(){
+    if (results.length > 0){
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
